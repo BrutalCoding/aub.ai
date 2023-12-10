0f7acff994884e5ca46879cfbfbf2d3a81941d47
BrutalCoding <daniel@brutalcoding.com>
2023-12-10 05:15:59 +0100

    fix: Missing Linux compatibility check and fix build process
    This commit adds a Linux compatibility check to the autoupdate script, ensuring it only runs on Linux systems. Additionally, the build process for generating native libraries has been updated to correctly navigate to the llama.cpp directory before building. The generated library is then copied to the appropriate location in the Linux project.

--- brutalcoding_scripts/autoupdate_linux.sh
+++ brutalcoding_scripts/autoupdate_linux.sh
@@ -6,6 +6,12 @@
 echo "[AUB.AI] Your system information:"
 echo "[AUB.AI] $(uname -a)"
 
+# Detect if OS is Linux, if not, exit the script.
+if [ "$(uname)" != "Linux" ]; then
+    echo "[AUB.AI] Sorry, this script only works on Linux. Exiting..."
+    exit 1
+fi
+
 # Navigate to root directory of the project
 echo "[AUB.AI] Navigating to root directory of the project..."
 cd ..
@@ -32,6 +38,7 @@ cd .. || exit
 # Generate a native libraries for the C++ code
 echo "[AUB.AI] Generating a native libraries for the C++ code..."
 cd src || exit
+cd llama.cpp || exit
 rm -rf build
 mkdir -p build
 cd build || exit
@@ -42,7 +49,7 @@ cmake --build . --config Release
 
 # Navigate back to the root directory
 echo "[AUB.AI] Navigating back to the root directory..."
-cd ../.. || exit
+cd ../../.. || exit
 
 # Re-check if its really the root directory of the project
 if [ ! -f "pubspec.yaml" ]; then
@@ -53,7 +60,7 @@ fi
 # Copy over the files to the Linux project
 echo "[AUB.AI] Adding llama.cpp to the Linux project..."
 mkdir -p linux/x64/lib
-cp src/build/llama.cpp/libllama.so linux/x64/lib/libllama.so || exit
+cp src/llama.cpp/build/libllama.so linux/x64/lib/libllama.so || exit
 
 # Generate all required files
 echo "[AUB.AI] Generating Dart files..."
@@ -62,12 +69,3 @@ dart run ffigen --config ffigen.yaml # Generate files for ffigen, see ffigen.yam
 
 # Notify user that AUB is ready to use.
 printf "[AUB.AI] Thanks for flying with Aub.ai today, the setup took off with success.\n\n"
-
-# Run the example app if the user wants to
-printf "[AUB.AI] Do you want to run the example app? (y/n): "
-read -r run_example
-if [ "$run_example" = "y" ]; then
-    printf "[AUB.AI] Running the example app...\n"
-    cd example || exit
-    flutter run
-fi
